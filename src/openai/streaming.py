9da5130a131211cc00639b87ecbb7804792b9a2d
James Morrissey <jpmorrissey11@gmail.com>
2023-09-29 16:30:46 -0400

    func for counting string tokens

--- src/openai/streaming.py
+++ src/openai/streaming.py
@@ -1,3 +1,6 @@
+import tiktoken
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+
 import openai
 
 
@@ -14,3 +17,9 @@ def stream_response(user_message, model_name):
         collected_chunks.append(chunk)
         chunk_message = chunk["choices"][0]["delta"]
         collected_messages.append(chunk_message)
+
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
